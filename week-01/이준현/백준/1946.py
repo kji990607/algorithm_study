f75a9824355ce51ab97e7d4918c598e972c27ac2
JunHyeon <dlwnsgus07@naver.com>
2022-07-10 23:14:52 +0900

    fix : 백준 1946번 코드

--- week-01/이준현/백준/1946.py
+++ week-01/이준현/백준/1946.py
@@ -1,20 +1,17 @@
 import sys
-case = int(input())
 
+case = int(input())
 for _ in range(case):
     number = int(input())
     interviewers = list()
-    count = 0
+    count = 1
     for _ in range(number):
         man = list(map(int, sys.stdin.readline().split()))
         interviewers.append(man)
     interviewers.sort()
-    for i in range(number):
-        flag = True
-        for j in range(i):
-            if interviewers[j][1] < interviewers[i][1]:
-                flag = False
-                break
-        if flag:
+    k = interviewers[0][1]
+    for i in range(1, number):
+        if k > interviewers[i][1]:
             count += 1
+            k = interviewers[i][1]
     print(count)
