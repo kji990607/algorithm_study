8b36a78374568487f087b1a2197ce46799430efd
JunHyeon <dlwnsgus07@naver.com>
2022-07-10 23:28:54 +0900

    fix : Q2

--- week-01/이준현/02/Q2.py
+++ week-01/이준현/02/Q2.py
@@ -1,13 +1,12 @@
 N, M = map(int, input().split())
 matrix = list()
-MAX = 0
 card = 0
 
 for _ in range(N):
     matrix.append(list(map(int, input().split())))
 
 for i in range(len(matrix)):
-    if MAX <= min(matrix[i]):
+    if card <= min(matrix[i]):
         card = min(matrix[i])
 
 print(card)
